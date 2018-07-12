2b397252426ebf3a8bceba324c18cc608828a277
Krhorsch <krhorsch@yahoo.com>
2018-07-12 16:06:46 -0400

    Done.

--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,37 @@
-// Code ThemedDecoration Component Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class ThemedDecorations extends React.Component {
+  debugger
+  render() {
+    const childrenWithClassName = React.Children.map(this.props.children, child => {
+      return React.cloneElement(child, {
+        className: this.props.theme
+      });
+    });
+    return (
+      <div className="themes-decorations">
+        {childrenWithClassName}
+      </div>
+    )
+  }
+}
+
+export default ThemedDecorations;
+
+// export default class MovieBrowser extends React.Component {
+//   render() {
+//     const currentPlayingTitle = 'Mad Max: Fury Road';
+//     const childrenWithExtraProp = React.Children.map(this.props.children, child => {
+//       return React.cloneElement(child, {
+//         isPlaying: child.props.title === currentPlayingTitle
+//       });
+//     });
+//
+//     return (
+//       <div className="movie-browser">
+//         {childrenWithExtraProp}
+//       </div>
+//     );
+//   }
+// }
